eb5eaa51c8db35b43123c2d531ed376b2f9d80ac
Looksh <lookshdev@gmail.com>
2023-11-14 20:39:47 +0900

    이거로

--- src/component/FooterComponent.js
+++ src/component/FooterComponent.js
@@ -1,10 +1,31 @@
 import "../style/FooterComponent.css";
+import React, { useEffect, useState } from "react";
 
 const FooterComponent = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    // 스크롤 이벤트를 감지하여 페이지가 스크롤될 때마다 상태를 업데이트
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="FooterComponent">
+    <footer className={isScrolled ? "scrolled" : ""}>
       <h1>Footer 입니다.</h1>
-    </div>
+    </footer>
   );
 };
 
